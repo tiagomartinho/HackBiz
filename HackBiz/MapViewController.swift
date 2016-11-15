dfd0d233f608c663a9ab402d39dad2e6082a9136
tiagomartinho <tiago.martinho@xpeppers.com>
2016-11-15 17:16:32 +0100

    draws polygon on map

--- HackBiz/MapViewController.swift
+++ HackBiz/MapViewController.swift
@@ -1,13 +1,29 @@
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView! {
+        didSet {
+            mapView.delegate = self
+        }
+    }
 
     var gpxURL: URL? {
         didSet {
             if let url = gpxURL {
                 GPX.parse(url) { gpx in
                     if gpx != nil {
-
+                        var coordinates = [CLLocationCoordinate2D]()
+                        if let first = gpx?.tracks.first {
+                            for fixe in first.fixes {
+                                coordinates.append(fixe.coordinate)
+                            }
+                        }
+                        let myPolyline = MKPolyline(coordinates: &coordinates, count: coordinates.count)
+                        DispatchQueue.main.async {
+                            self.mapView.add(myPolyline)
+                        }
                     }
                 }
             }
@@ -18,5 +34,14 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
         let urlpath = Bundle.main.path(forResource: "tappa-01-dal-gran-s-bernardo-echevennoz", ofType: "gpx")
         gpxURL = NSURL.fileURL(withPath: urlpath!)
+    }
+
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKPolyline {
+            let lineView = MKPolylineRenderer(overlay: overlay)
+            lineView.strokeColor = UIColor.green
+            return lineView
+        }
+        return MKOverlayRenderer()
     }
 }
