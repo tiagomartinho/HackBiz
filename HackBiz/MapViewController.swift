2eb289751d07dd9eed1fd1583b14a6a073f25cd2
tiagomartinho <tiago.martinho@xpeppers.com>
2016-11-16 03:33:32 +0100

    calculates routes

--- HackBiz/MapViewController.swift
+++ HackBiz/MapViewController.swift
@@ -18,7 +18,12 @@ class MapViewController: UIViewController, MKMapViewDelegate {
                     if gpx != nil {
                         var coordinates = [CLLocationCoordinate2D]()
                         if let first = gpx?.tracks.first {
-                            self.tracks.append(first.fixes.first!)
+                            var track = first.fixes.first!
+                            track.info = first.fixes.description
+                            self.tracks.append(track)
+                            DispatchQueue.main.async {
+                                self.mapView.addAnnotation(track)
+                            }
                             for fixe in first.fixes {
                                 coordinates.append(fixe.coordinate)
                             }
@@ -76,8 +81,70 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     }
 
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        let location = CLLocation(latitude: (view.annotation?.coordinate.latitude)!,
+                                   longitude: (view.annotation?.coordinate.longitude)!)
+        let waypoint = nearest(location: location, waypoints: tracks)
+        let sourceLocation = CLLocationCoordinate2D(latitude: (waypoint?.latitude)!, longitude: (waypoint?.longitude)!)
+        let destinationLocation = location.coordinate
 
+        let sourcePlacemark = MKPlacemark(coordinate: sourceLocation, addressDictionary: nil)
+        let destinationPlacemark = MKPlacemark(coordinate: destinationLocation, addressDictionary: nil)
+
+        let sourceMapItem = MKMapItem(placemark: sourcePlacemark)
+        let destinationMapItem = MKMapItem(placemark: destinationPlacemark)
+
+        let sourceAnnotation = MKPointAnnotation()
+        sourceAnnotation.title = "Times Square"
+
+        if let location = sourcePlacemark.location {
+            sourceAnnotation.coordinate = location.coordinate
+        }
+
+
+        let destinationAnnotation = MKPointAnnotation()
+        destinationAnnotation.title = "Empire State Building"
+
+        if let location = destinationPlacemark.location {
+            destinationAnnotation.coordinate = location.coordinate
+        }
+
+        self.mapView.showAnnotations([sourceAnnotation,destinationAnnotation], animated: true )
+
+        let directionRequest = MKDirectionsRequest()
+        directionRequest.source = sourceMapItem
+        directionRequest.destination = destinationMapItem
+        directionRequest.transportType = .walking
+
+        let directions = MKDirections(request: directionRequest)
+        directions.calculate {
+            (response, error) -> Void in
+
+            guard let response = response else {
+                if let error = error {
+                    print("Error: \(error)")
+                }
+
+                return
+            }
+
+            let route = response.routes[0]
+            self.mapView.add((route.polyline), level: .aboveRoads)
+
+            let rect = route.polyline.boundingMapRect
+            self.mapView.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
+        }
     }
 
-    
+    func nearest(location: CLLocation?,
+                        waypoints: [Waypoint]) -> Waypoint? {
+        guard let location = location else { return nil }
+
+        return waypoints.min { w1, w2 in
+                let location1 = CLLocation(latitude: w1.latitude,
+                                           longitude: w1.longitude)
+                let location2 = CLLocation(latitude: w2.latitude,
+                                           longitude: w2.longitude)
+                return location.distance(from: location1) < location.distance(from: location2)
+        }
+    }
 }
